e9c4fac556718505a819b5bdb5da0a380bf4e9d0
nomennescio1 <u1201416@unimilitar.edu.co>
2018-04-11 17:20:05 -0500

    updated to bs 4 and made it work with enter press

--- portfolio/wiki/index.js
+++ portfolio/wiki/index.js
@@ -1,31 +1,28 @@
-$(function(){
   
   $('#search').on('click', function(){
-  $('#fillme').empty();
-  var busqueda = $('#term').val();
-  var url ="https://en.wikipedia.org/w/api.php?action=opensearch&search="+ busqueda +"&format=json&callback=?"; 
-    
-    $.ajax({
-      url: url,
-      type: 'GET',
-      contentType: "application/json; charset=utf-8",
-			async: false,
-      dataType: "json",
-      success: function(data){
-        for(var i=0;i<data[1].length;i++){
-         $("#fillme").append("<div class='card'> <h2 class='card-title'> <a href=" + data[3][i] + " target='_blank'>" + data[1][i]+  " </a></h2> " + "<div class ='card-block'><p class='card-text'>" + data[2][i] + "</p></div></div><br>");
+    $('#fillme').empty();
+    var busqueda = $('#term').val();
+    var url ="https://en.wikipedia.org/w/api.php?action=opensearch&search="+ busqueda +"&format=json&callback=?"; 
+      
+      $.ajax({
+        url: url,
+        type: 'GET',
+        contentType: "application/json; charset=utf-8",
+        async: false,
+        dataType: "json",
+        success: function(data){
+          for(var i=0;i<data[1].length;i++){
+           $("#fillme").append("<div class='card'> <h2 class='card-title'> <a href=" + data[3][i] + " target='_blank'>" + data[1][i]+  " </a></h2> " + "<div class ='card-block'><p class='card-text'>" + data[2][i] + "</p></div></div><br>");
+          }
         }
-      }
-   
+     
+      });
+      
     });
-    
-  });
-   
-});
-$(document).ready(function(){
-  $("#input").keyup(function(event) {
-    if (event.keyCode == 13) {
-      $("#search").click();
-    }
-  });
-});
+  
+    $("#term").keyup(function(event) {
+      if (event.keyCode === 13) {
+        $("#search").click();
+      }
+    });
+  
