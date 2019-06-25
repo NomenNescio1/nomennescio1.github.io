08744ea0f573d7f41cfc2db93dda5406ccb94967
Daniel <u1201416@unimilitar.edu.co>
2019-06-25 17:30:55 -0500

    trying with no jquery

--- portfolio/twitch-v2/app.js
+++ portfolio/twitch-v2/app.js
@@ -1,4 +1,4 @@
-$.ajax({
+/*$.ajax({
     type: 'GET',
     url: 'https://api.twitch.tv/helix/streams?first=20',
     headers: {
@@ -7,4 +7,33 @@ $.ajax({
     success: function(data) {
       console.log(data);
     }
-   });
+   });*/
+
+(function() {
+  var httpRequest;
+  document.getElementById("ajaxButton").addEventListener('click', makeRequest);
+
+  function makeRequest() {
+    httpRequest = new XMLHttpRequest();
+    httpRequest.setRequestHeader('Client-ID', '79ctfw3yfefdxfek4gto3q1e06mnkv')
+
+    if (!httpRequest) {
+      alert('Giving up :( Cannot create an XMLHTTP instance');
+      return false;
+    }
+    httpRequest.onreadystatechange = alertContents;
+    httpRequest.open('GET', 'https://api.twitch.tv/helix/streams?first=20');
+    httpRequest.send();
+  }
+
+  function alertContents() {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+      if (httpRequest.status === 200) {
+        console.log(httpRequest.responseText);
+      } else {
+        alert('There was a problem with the request.');
+      }
+    }
+  }
+})();
+   
